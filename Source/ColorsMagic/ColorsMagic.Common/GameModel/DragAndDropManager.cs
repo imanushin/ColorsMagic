c6959b09c073e1e0949b56720003df1ae6a75c9e
Igor Manushin <igor.manushin@optimarea.com>
2016-01-24 01:12:49 +0300

    add analyzing package

--- Source/ColorsMagic/ColorsMagic.Common/GameModel/DragAndDropManager.cs
+++ Source/ColorsMagic/ColorsMagic.Common/GameModel/DragAndDropManager.cs
@@ -1,31 +1,34 @@
-﻿using System.Collections.Immutable;
+﻿using JetBrains.Annotations;
+using System.Collections.Immutable;
 
 namespace ColorsMagic.Common.GameModel
 {
     public sealed class DragAndDropManager
     {
         private readonly GridGenerator _gridGenerator;
+        private readonly GameColorViewModel _colorsModel;
 
-        public DragAndDropManager(GridGenerator gridGenerator)
+        public DragAndDropManager([NotNull] GridGenerator gridGenerator, [NotNull] GameColorViewModel colorsModel)
         {
             _gridGenerator = gridGenerator;
+            _colorsModel = colorsModel;
         }
 
         public ImmutableArray<PortablePoint> MovePath { get; }
 
         public void StartDrag(TrianglePosition initialBall)
         {
-            
+
         }
 
-        public void ContinueDrag(PortablePoint currentPoint)
+        public void ContinueDrag([NotNull] PortablePoint currentPoint)
         {
-            
+
         }
 
         public void FinishDrag()
         {
-            
+
         }
     }
 }
